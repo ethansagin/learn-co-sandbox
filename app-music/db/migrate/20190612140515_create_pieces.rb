18f0b180724bf4101fd946f0ec03df7db4adefb3
Ethan Sagin <ethansagin@gmail.com>
2019-06-12 14:12:11 +0000

    migrate tables

---
+++ app-music/db/migrate/20190612140515_create_pieces.rb
@@ -0,0 +1,14 @@
+class CreatePieces < ActiveRecord::Migration
+  def change
+    create_table :pieces do |t|
+      t.string :name
+      t.string :larger_work
+      t.string :genre
+      t.string :composer
+      t.string :librettist
+      t.string :translation
+      t.string :publisher
+      t.string :in_use?
+    end
+  end
+end
